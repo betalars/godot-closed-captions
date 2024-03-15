457dcc6e70e913ca029e75bc63b9c572448e18a4
betalars <contact@betalars.de>
2024-03-15 15:31:01 +0100

    implementing baked caption for internal use in server

--- addons/closed_captions/caption_server.gd
+++ addons/closed_captions/caption_server.gd
@@ -1,1 +1,15 @@
 extends Node
+class_name CaptionServer
+
+var sources: Array[Array] = [[],[]]
+var captions: Array[ServerCaption]
+
+
+func update_source(source: CaptionedAudioStreamPlayer):
+    var listener = source.get_viewport()
+    if !sources[0].has(listener):
+        sources[0].append(listener)
+    sources[1][sources[0].find(listener)].append(source)
+
+func push_caption(source: Node, caption: Caption):
+    pass
