5dadddcb8c284cb0c8ef3443ebb6d556cf031142
betalars <contact@betalars.de>
2024-03-17 22:52:15 +0100

    loading CaptionServer

--- addons/closed_captions/caption_plugin.gd
+++ addons/closed_captions/caption_plugin.gd
@@ -2,7 +2,9 @@
 extends EditorPlugin
 
 func _enter_tree():
+	add_autoload_singleton("CaptionServer", "res://addons/closed_captions/caption_server.gd")
 	add_custom_type("CaptionedAudioStreamPlayer", "AudioStreamPlayer", preload("res://addons/closed_captions/captioned_autio_stream_player.gd"), preload("icon.svg"))
 
 func _exit_tree():
 	remove_custom_type("CaptionedAudioStreamPlayer")
+	remove_autoload_singleton("CaptionServer")
