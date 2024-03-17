9572e59abed832409979de4ed10c613041d3ccc6
betalars <contact@betalars.de>
2024-03-17 22:52:30 +0100

    WIP: implementing caption server

--- addons/closed_captions/caption_server.gd
+++ addons/closed_captions/caption_server.gd
@@ -1,15 +1,21 @@
 extends Node
-class_name CaptionServer
 
 var sources: Array[Array] = [[],[]]
 var captions: Array[ServerCaption]
 
+func push_caption(source: CaptionedAudioStreamPlayer, caption: Caption):
+	if not AudioServer.is_bus_mute(AudioServer.get_bus_index(source.bus)):
+		if is_listening(source.get_viewport()):
+			var server_caption:ServerCaption = ServerCaption.new(caption)
+			captions.append(server_caption)
+			await get_tree().create_timer(caption._duration-0.05).timeout
+			captions.erase(server_caption)
 
-func update_source(source: CaptionedAudioStreamPlayer):
-	var listener = source.get_viewport()
-	if !sources[0].has(listener):
-		sources[0].append(listener)
-	sources[1][sources[0].find(listener)].append(source)
-
-func push_caption(source: Node, caption: Caption):
-	pass
+func is_listening(listener: Viewport):
+	if not (listener.audio_listener_enable_2d or listener.audio_listener_enable_3d):
+		return false
+	else:
+		if listener == get_viewport():
+			return true
+		else:
+			return is_listening(listener.get_viewport())
