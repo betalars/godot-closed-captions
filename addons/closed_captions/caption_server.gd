ed103550f93a8cac000d1b8365aaef9818b7292e
betalars <contact@betalars.de>
2024-03-29 16:54:11 +0100

    WIP: implementing display for captions

--- addons/closed_captions/caption_server.gd
+++ addons/closed_captions/caption_server.gd
@@ -1,15 +1,18 @@
 extends Node
 
 var sources: Array[Array] = [[],[]]
-var captions: Array[ServerCaption]
+var caption_displays: Array[CaptionDisplay]
 
 func push_caption(source: CaptionedAudioStreamPlayer, caption: Caption):
 	if not AudioServer.is_bus_mute(AudioServer.get_bus_index(source.bus)):
 		if is_listening(source.get_viewport()):
-			var server_caption:ServerCaption = ServerCaption.new(caption)
-			captions.append(server_caption)
-			await get_tree().create_timer(caption._duration-0.05).timeout
-			captions.erase(server_caption)
+			var outputs:Array[CaptionDisplay]
+			for display in caption_displays:
+				if display.is_receiving_bus(source.bus): outputs.append(display)
+			outputs.sort_custom(func(a:CaptionDisplay, b:CaptionDisplay) -> bool: return a.priority > b.priority)
+			outputs[0].display_caption(caption)
+
+func pull_caption(caption: Caption): for display in caption_displays: display.pull_caption(caption)
 
 func is_listening(listener: Viewport):
 	if not (listener.audio_listener_enable_2d or listener.audio_listener_enable_3d):
@@ -19,3 +22,10 @@ func is_listening(listener: Viewport):
 			return true
 		else:
 			return is_listening(listener.get_viewport())
+
+func signup_display(new: CaptionDisplay):
+	if not caption_displays.has(new):
+		caption_displays.append(new)
+	
+func signoff_display(old: CaptionDisplay):
+	caption_displays.erase(old)
---
+++ addons/closed_captions/fullscreen_caption_display.gd
@@ -0,0 +1,58 @@
+@tool
+extends MarginContainer
+class_name CaptionDisplay
+
+var _display: VBoxContainer = VBoxContainer.new()
+
+var speakers: Dictionary = {}
+
+@export var displaying: Array[Caption] = []:
+	set(display):
+		displaying = display
+		print("gothere")
+@export_flags_2d_physics var source_bus: int = 2
+@export_range(0, 100) var priority: float = 10
+
+@export_range(0, 100) var side_margin:int = 50:
+	set(margin):
+		side_margin = margin
+		add_theme_constant_override("margin_left", side_margin)
+		add_theme_constant_override("margin_right", side_margin)
+
+@export_range(0, 100) var bottom_margin:int = 25:
+	set(margin):
+		bottom_margin = margin
+		add_theme_constant_override("margin_bottom", bottom_margin)
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray
+	# get_viewport_rect in editor mode returns the editor viewport, therefore we need this workaround:
+	var screen_size = Rect2(Vector2(), Vector2(ProjectSettings.get_setting("display/window/size/viewport_width"), ProjectSettings.get_setting("display/window/size/viewport_height")))
+	if not get_rect().is_equal_approx(screen_size):
+		warnings.append("This node is intended to be used covering the entire screen. Use Compact Captions Container instead.")
+	if get_child_count() != 0: warnings.append("Do not attatch children to Captions Container.")
+	return warnings
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_child(_display, false, Node.INTERNAL_MODE_BACK)
+	_display.alignment = BoxContainer.ALIGNMENT_END
+	
+	CaptionServer.signup_display(self)
+
+func _exit_tree():
+	CaptionServer.signoff_display(self)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func display_caption(caption: Caption, previous: Caption = null):
+	pass
+
+func pull_caption(caption: Caption):
+	if displaying.has(caption):
+		displaying.erase(caption)
+
+func is_receiving_bus(bus: StringName):
+	return (source_bus & 2 ** AudioServer.get_bus_index(bus)) > 0
