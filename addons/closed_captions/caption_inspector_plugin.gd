f7de1c230041a24f62760bc2feef2bb7c4df2a3b
betalars <contact@betalars.de>
2024-09-09 16:39:03 +0200

    updating logic so displaying panel is more consistent

--- addons/closed_captions/caption_inspector_plugin.gd
+++ addons/closed_captions/caption_inspector_plugin.gd
@@ -3,15 +3,11 @@ class_name CaptionInspectorPlugin
 extends EditorInspectorPlugin
 #var CaptionEditor = preload("res://addons/closed_captions/caption_inspector_editor.gd")
 
-var just_called:bool = false
-
 func _can_handle(object):
 	if object is CaptionedAudioStreamPlayer or object is CaptionedAudioStreamPlayer3D or object is CaptionedAudioStreamPlayer2D:
 		PluginReference.plugin.display_caption_panel_for(object)
-		just_called = true
 	else:
-		if just_called:
-			just_called = false
-		else:
-			PluginReference.plugin.hide_caption_panel()
+		if object is Node:
+			if not object.name.contains("Editor"):
+				PluginReference.plugin.hide_caption_panel()
 	return false
