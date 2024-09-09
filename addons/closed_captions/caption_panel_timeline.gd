132cca127e09ea7ee7b4f4a6cff830ab4ccb7c85
betalars <contact@betalars.de>
2024-09-09 16:55:55 +0200

    WIP: implementing display and handling of captions in a timeline

--- addons/closed_captions/caption_panel_timeline.gd
+++ addons/closed_captions/caption_panel_timeline.gd
@@ -1,29 +1,42 @@
-extends VBoxContainer
+@tool
+class_name CaptionPanelTimeline extends VBoxContainer
 
-var minute_size:float = 64
+var minute_size:float:
+	set(no):
+		assert(false)
+	get():
+		if is_node_ready():
+			return (total_pixels/time_scale)/60
+		else:
+			return 42
 var caption_stacks: Array[Array]
 
-@export_range(0.1, 2) var current_time_scale = 1:
-	set(time_scale):
-		if time_scale >= 0 and time_scale <= 1:
-			current_time_scale = time_scale
-		elif time_scale <= 2:
-			current_time_scale = time_scale**2
+@export_range(0.1, 2) var time_scale:float = 1:
+	set(value):
+		if value == null: return
 		
-		if is_node_ready():
-			minute_size = total_pixels/current_time_scale
-@onready var total_pixels:int = size.x
+		if value >= 0 and value <= 1:
+			time_scale = value
+		elif time_scale <= 2:
+			time_scale = value**2
+		populate()
+
+@onready var total_pixels:int = get_viewport().get_visible_rect().size.x-128
 
 @export var time_begin: float
-@export var time_end: float
+@export var time_end: float:
+	set(no):
+		assert(false)
+	get():
+		return time_begin + (time_scale * 60)
 @export var time_cursor: float
 @export var multi_caption_stream: MultiCaptionAudioStream:
 	set(new):
 		if multi_caption_stream!= null:
-			multi_caption_stream.caption_changed.disconnect(populate)
+			multi_caption_stream.captions_changed.disconnect(populate)
 		multi_caption_stream = new
 		if multi_caption_stream!= null:
-			multi_caption_stream.caption_changed.connect(populate)
+			multi_caption_stream.captions_changed.connect(populate)
 			populate()
 @onready var time_labels: HBoxContainer = $TimeLabels
 @onready var time_selector: HSlider = $TimeSelector
@@ -33,14 +46,8 @@ var caption_stacks: Array[Array]
 signal caption_reached(captin: Caption)
 signal caption_selected(captin: Caption)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func select_time_scale(value:float) -> void:
+	time_scale = value
 
 func get_readable_time(mins: float) -> String:
 	if mins > 1:
@@ -48,11 +55,22 @@ func get_readable_time(mins: float) -> String:
 	else:
 		return "%d:%d" % [floor(mins), floor(fmod(mins, 1) * 60)]
 
+func fill_timeline():
+	var seperator:VSeparator
+	var label
+	
+	for i in range(2):
+		if get_child(i) is VSeparator:
+			seperator = get_child(i)
+		else:
+			label = get_child(i)
+
+	for child in get_children():
+		remove_child(child)
+
 func populate() -> void:
 	if is_node_ready():
 		generate_stack()
-	
-		print(caption_stacks)
 		
 		for child in button_timelines.get_children():
 			button_timelines.remove_child(child)
@@ -63,18 +81,22 @@ func populate() -> void:
 			button_timelines.add_child(row)
 			
 			for i in range(stack.size()):
-				row.add_child(CaptionPanelTimelineButton.new(stack[i], minute_size, time_end if i == stack.size()-1 else stack[i+1].delay))
+				if stack[i].delay > time_end: break
+				var captionButton:= CaptionPanelButton.new(stack[i], minute_size, min(time_end, minute_size*0.1) if i == stack.size()-1 else stack[i+1].delay)
+				captionButton.pressed.connect(multi_caption_stream.set.bind("caption", captionButton.caption))
+				row.add_child(captionButton)
 				if stack[i].duration != 0:
 					if i < stack.size()-2:
 						if stack[i].duration + stack[i].delay > stack[i+1].delay:
 							var spacer = VSeparator.new()
 							spacer.custom_minimum_size.x = (min(stack[i+1].delay, time_end) - stack[i].duration - stack[i].delay) * minute_size
 							row.add_child(spacer)
+		
+		time_selector.min_value = time_begin
+		time_selector.max_value = time_end
 
 func generate_stack() -> void:
 	caption_stacks = [[]]
-	
-	print(multi_caption_stream._captions_array)
 	
 	var depth = 0
 	var last_end = -1
